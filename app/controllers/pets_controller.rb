7644fa1ccae2c54b556d8bd0fe3a962c4987a372
jinook929 <jinook929@gmail.com>
2021-02-25 19:05:31 -0500

    commit tree b88af4fdc1711d4e6831ba5f8a71879e7649b198

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -6,21 +6,41 @@ class PetsController < ApplicationController
   end
 
   get '/pets/new' do 
+    @owners = Owner.all
     erb :'/pets/new'
   end
 
   post '/pets' do 
-
-    redirect to "pets/#{@pet.id}"
+    # owner = Owner.find_by(id: params[:pet][:owner_id]) || Owner.create(params[:owner])
+    # pet = Pet.create(name: params[:pet][:name], owner_id: owner.id)
+    pet = Pet.create(params[:pet])
+    if !params[:owner][:name].empty?
+      pet.owner = Owner.create(params[:owner])
+      pet.save
+    end
+    redirect to "pets/#{pet.id}"
   end
 
   get '/pets/:id' do 
-    @pet = Pet.find(params[:id])
+    @pet = Pet.find_by(id: params[:id])    
     erb :'/pets/show'
   end
 
-  patch '/pets/:id' do 
+  get '/pets/:id/edit' do
+    @owners = Owner.all
+    @pet = Pet.find_by(id: params[:id])
+    erb :'pets/edit'
+  end
 
+  patch '/pets/:id' do 
+    @pet = Pet.find_by(id: params[:id])
+    @pet.update(params[:pet])
+
+    if !params[:owner][:name].empty?
+      @pet.owner = Owner.create(name: params[:owner][:name])
+      @pet.save
+    end
+    
     redirect to "pets/#{@pet.id}"
   end
 end
